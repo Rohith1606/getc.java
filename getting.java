f7fb66be443a73135300e5cfca98d3a2c3938053
Rohith1606 <61485350+Rohith1606@users.noreply.github.com>
2020-03-03 15:28:25 +0530

    Create getting.java

---
+++ getting.java
@@ -0,0 +1,23 @@
+package Mathissues;
+import java.util.Scanner;
+
+public class Findfactorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, c, f = 1;
+    	System.out.println("Enter an integer to calculate its factorial");
+    	Scanner in = new Scanner(System.in);
+    	n = in.nextInt();
+    	if (n < 0)
+      		System.out.println("Number should be non-negative.");
+    	else
+    {
+      	for (c = 1; c <= n; c++)
+        	f = f*c;
+      		System.out.println("Factorial of "+n+" is = "+f);
+    		}
+  	}
+
+	}
+
